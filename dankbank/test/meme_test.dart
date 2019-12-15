fd43adb476c219e93300e51466a49739cfb39da4
bolson10 <bolson10@mail.csuchico.edu>
2019-12-15 11:21:32 -0800

    Adding unit testing and coverage to app

--- dankbank/test/meme_test.dart
+++ dankbank/test/meme_test.dart
@@ -1,19 +1,35 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:dankbank/meme.dart';
-import 'dart:convert';
 import 'package:test/test.dart';
 
 void main() {
-  test('Sanity check', () {
-    var result = true;
-    rootBundle.loadString('assets/hardcode.json').then(
-            (str) {
-          List memes = jsonDecode(str);
-         // _suggestions.addAll(memes.map((m) => Meme.fromJson(m)).toList());
-          MemeList.fromJson(memes);
-        }
-    );
-    expect(result, true);
+  /*
+  test('Check toJson in MemeList', () {
+    Meme m1 = Meme(username: 'bobby',url: 'www.google.com',text: 'Can we all just get A\'s?');
+    expect(m1.toJson(), '{username: bobby, url: www.google.com, text: Can we all just get A\'s?}');
+  });
+   */
+  test('Check hasData variable in MemeList Data', () {
+    Meme m1 = Meme(username: 'a',url: 'a',text: 'a');
+    Meme m2 = Meme(username: 'a',url: 'a',text: 'a');
+    List memes = [m1.toJson(),m2.toJson()];
+    MemeList a = MemeList.fromJson(memes);
+    expect(a.hasData, true);
+  });
+  test('Check hasData variable in MemeList No Data', () {
+    List memes = [];
+    MemeList a = MemeList.fromJson(memes);
+    expect(a.hasData, false);
+  });
+  test('Check fromJson checks null parameter', () {
+    List memes = null;
+    expect(MemeList.fromJson(memes), null);
+  });
+  test('Check fromJson parsing in MemeList', () {
+    Meme m1 = Meme(username: 'bobby',url: 'www.google.com',text: 'Can we all just get A\'s?');
+    Meme m2 = Meme(username: 'jimbo',url: 'www.google.com',text: 'Epstein didn\'t kill himself');
+    Meme m3 = Meme(username: 'gus',url: 'www.google.com',text: 'Milk.');
+    List memes = [m1.toJson(),m2.toJson(),m3.toJson()];
+    MemeList a = MemeList.fromJson(memes);
+    expect(a.memes[1].text, 'Epstein didn\'t kill himself');
   });
 }
